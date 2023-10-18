f9077ef8c10b53ebc6ed75823537e04cd5917fb3
somicodes12 <somtochukwue98@gmail.com>
2023-10-18 17:01:56 +0100

    somto's push

--- src/Pages/Home/Testimony.tsx
+++ src/Pages/Home/Testimony.tsx
@@ -1,8 +1,74 @@
-import React from 'react'
-
 const Testimony = () => {
   return (
-    <div>Testimony</div>
+    <div>
+      <div className="w-full h-[auto]  p-6 pl-4 flex justify-between small:flex-col ">
+      <div className=" w-[45%] h-full bg-lue-600 small:w-full ">
+        <div className="py-4 mx-12 meduim:w-full">
+          <div className="w-[390px] h-[300px]  ">
+            <div className="text-green-500 ">CLIENT'S FEEDBACK</div>
+            <div className="text-[35px] my-6 font-bold ">What People Say</div>
+            <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti rem quasi nemo ab unde debitis, dolorem ut id est sed provident sit similique excepturi iure!</div>
+            <div className="w-[170px] h-[50px] bg-green-00 my-4 rounded-[30px] text-white flex justify-center items-center bg-green-500">view all reviewers</div>
+          </div>
+          <div className="w-full h-[200px] border mt-[50px] p-6 font-bold meduim:w-[100%]">
+            <div>Most Popular Clients</div>
+          </div>
+        </div>
+        <div></div>
+      </div>
+      <div className="bg-yellow-00 w-[64%] h-full  flex-wrap small:full meduim:justify-center">
+        <div className="w-[780px] h-[520px]  mx-[30px] flex flex-wrap small:w-[620px]  relative ">
+          <div className="w-[330px] h-[290px] border absolute bottom-12 left-[220px] bg-slate-100 -z-30 small:hidden"></div>
+          <div className="w-[330px] h-[290px] border m-7  p-4 shadow-md ">
+            <div className="flex p-4 text-[17px]">
+              <div className="w-[50px] h-[50px] rounded-[50%] border mr-4"></div>
+              <div className="small:text-[18px]">
+                <div className="font-bold">Shirley Smith</div>
+                <div>Founder & CEO</div>
+              </div>
+            </div>
+            <div className="my-4  small:text-[20px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde voluptatibus!</div>
+            <div></div>
+          </div>
+          <div className="w-[330px] h-[290px] border m-7  p-4 shadow-md ">
+            <div className="flex p-4 text-[17px]">
+              <div className="w-[50px] h-[50px] rounded-[50%] border mr-4"></div>
+              <div className="small:text-[18px]">
+                <div className="font-bold">Shirley Smith</div>
+                <div>Founder & CEO</div>
+              </div>
+            </div>
+            <div className="my-4  small:text-[20px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde voluptatibus!</div>
+            <div></div>
+          </div>
+          <div className="w-[330px] h-[290px] border m-7  p-4 shadow-md ">
+            <div className="flex p-4 text-[17px]">
+              <div className="w-[50px] h-[50px] rounded-[50%] border mr-4"></div>
+              <div className="small:text-[18px]">
+                <div className="font-bold">Shirley Smith</div>
+                <div>Founder & CEO</div>
+              </div>
+            </div>
+            <div className="my-4  small:text-[20px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde voluptatibus!</div>
+            <div></div>
+          </div>
+          <div className="w-[330px] h-[290px] border m-7  p-4 shadow-md ">
+            <div className="flex p-4 text-[17px]">
+              <div className="w-[50px] h-[50px] rounded-[50%] border mr-4"></div>
+              <div className="small:text-[18px]">
+                <div className="font-bold">Shirley Smith</div>
+                <div>Founder & CEO</div>
+              </div>
+            </div>
+            <div className="my-4  small:text-[20px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde voluptatibus!</div>
+            <div></div>
+          </div>
+   
+        
+        </div>
+      </div>
+      </div>
+    </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      screens:{
-        small: {max: "768px"},
-        meduim: {max: "320px"},
-      }
+      screens: {
+        small: { max: "768px" },
+        meduim: { max: "320px" },
+      },
     },
   },
   plugins: [],
-}
-
+};
